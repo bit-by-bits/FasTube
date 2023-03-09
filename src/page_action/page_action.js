54b9e0ad79336b623ce490cccbfc9290ada759ce
Prateek Kashyap <kashyapprateek13@gmail.com>
2023-03-09 11:05:14 +0530

    chore: style changes

--- src/page_action/page_action.js
+++ src/page_action/page_action.js
@@ -1,14 +1,13 @@
 document.getElementById("input-form").addEventListener("submit", (e) => {
   e.preventDefault();
-
-  let params = {
-    currentWindow: true,
-    active: true,
-  };
-
-  function gotTabs(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {"message": `${document.getElementById('input-text').value}`});
-  }
-
-  chrome.tabs.query(params, gotTabs);
+  chrome.tabs.query(
+    {
+      currentWindow: true,
+      active: true,
+    },
+    (tabs) =>
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: `${document.getElementById("input-text").value}`,
+      })
+  );
 });
